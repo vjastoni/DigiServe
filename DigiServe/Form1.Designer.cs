cadd908940d3fd282aed90e01c478e038730ca18
Jastoni <47589812+vjastoni@users.noreply.github.com>
2021-11-23 12:59:53 +0800

    try lang ulet

--- DigiServe/Form1.Designer.cs
+++ DigiServe/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace DigiServe
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Hotel De Paris", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(231, 9);
+            this.label1.Location = new System.Drawing.Point(257, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(284, 64);
             this.label1.TabIndex = 0;
