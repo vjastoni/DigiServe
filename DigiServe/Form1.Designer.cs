ce7a6ded919d7eac897f01cc2488ca445b1229fa
Jastoni <47589812+vjastoni@users.noreply.github.com>
2021-11-23 13:01:54 +0800

    Sarap mo pia

--- DigiServe/Form1.Designer.cs
+++ DigiServe/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace DigiServe
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Hotel De Paris", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(257, 25);
+            this.label1.Location = new System.Drawing.Point(253, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(284, 64);
             this.label1.TabIndex = 0;
@@ -49,7 +49,7 @@ namespace DigiServe
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DigiServe";
             this.ResumeLayout(false);
             this.PerformLayout();
 
